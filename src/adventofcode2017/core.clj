5fab4216442230deafd36bf768c18ac5574f0f6e
Bob Williams <bobwilliams.ii@gmail.com>
2017-12-13 10:56:29 -0500

    making it a bit more programmatic

--- src/adventofcode2017/core.clj
+++ src/adventofcode2017/core.clj
@@ -1,13 +1,17 @@
 (ns adventofcode2017.core
   (:gen-class)
-  (:require [adventofcode2017.day1 :as day1]
-            [adventofcode2017.day2 :as day2]
-            [adventofcode2017.day3 :as day3]
-            [adventofcode2017.day4 :as day4]
-            [adventofcode2017.day5 :as day5]))
+  (:require [adventofcode2017.day1]
+            [adventofcode2017.day2]
+            [adventofcode2017.day3]
+            [adventofcode2017.day4]
+            [adventofcode2017.day5]))
 
-(def days [day1/solution day2/solution day3/solution day4/solution day5/solution])
+(def max-solutions 6)
+
+(def solutions 
+  (->>  (range 1 max-solutions)
+        (map #(symbol (str "adventofcode2017.day" %) "solution"))))
 
 (defn -main [& args]
-  (dorun (map #(apply % nil) days))
+  (dorun (map #(@(resolve %)) solutions))
   (println "done!"))
